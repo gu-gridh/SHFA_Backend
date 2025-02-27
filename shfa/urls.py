5ed34afbeeb0d46fd64c5acc61bbb5c0e6520e42
AramKarimi <aram.karimi@gu.se>
2025-02-27 11:20:44 +0100

    remove utils class

--- shfa/urls.py
+++ shfa/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from shfa.utils import build_contact_form_endpoint
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
