1bba989c1290554784a5036774e97db6ad0f9e18
AramKarimi <aram.karimi@gu.se>
2025-02-27 13:47:33 +0100

    upload templates for OAI

--- shfa/urls.py
+++ shfa/urls.py
@@ -1,4 +1,4 @@
-"""diana URL Configuration
+"""SHFA URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
