68dcf9f1b82d8efe0f1b95dc8028ccb40de332d7
AramKarimi <aram.karimi@gu.se>
2025-08-04 13:57:57 +0200

    test

--- apps/shfa/forms.py
+++ apps/shfa/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.validators import EmailValidator
-
+# Contact form for the SHFA application
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
