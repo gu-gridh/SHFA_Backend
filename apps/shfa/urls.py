c12ed02d38446c1527d6d6620778f5d34e8da8b3
AramKarimi <aram.karimi@gu.se>
2025-08-05 10:20:27 +0200

    update urls

--- apps/shfa/urls.py
+++ apps/shfa/urls.py
@@ -8,52 +8,52 @@ router = routers.DefaultRouter()
 endpoint = utils.build_app_endpoint("shfa")
 documentation = utils.build_app_api_documentation("shfa", endpoint)
 
-# router.register(rf'{endpoint}/image', views.IIIFImageViewSet, basename='image')
-# router.register(rf'{endpoint}/site', views.SiteViewSet, basename='site')
-# router.register(rf'{endpoint}/geojson/site',
-#                 views.SiteGeoViewSet, basename='site as geojson')
-# router.register(rf'{endpoint}/compilation',
-#                 views.CompilationViewset, basename='compilation')
+router.register(rf'{endpoint}/image', views.IIIFImageViewSet, basename='image')
+router.register(rf'{endpoint}/site', views.SiteViewSet, basename='site')
+router.register(rf'{endpoint}/geojson/site',
+                views.SiteGeoViewSet, basename='site as geojson')
+router.register(rf'{endpoint}/compilation',
+                views.CompilationViewset, basename='compilation')
 
 # urls for advanced search options
-# router.register(rf'{endpoint}/search/site',
-#                 views.SiteSearchViewSet, basename='search site')
-# router.register(rf'{endpoint}/search/image',
-#                 views.SearchBoundingBoxImageViewSet, basename='bounding box image')
-# router.register(rf'{endpoint}/search/type',
-#                 views.TypeSearchViewSet, basename='site type')
-# router.register(rf'{endpoint}/search/keywords',
-#                 views.SearchKeywords, basename='keywords')
-# router.register(rf'{endpoint}/search/carving',
-#                 views.SearchRockCarving, basename='search rock carving')
-# router.register(rf'{endpoint}/search/author',
-#                 views.SearchPeople, basename='search for image creator')
-# router.register(rf'{endpoint}/search/institution',
-#                 views.SearchInstitution, basename='institution')
-# router.register(rf'{endpoint}/search/dating',
-#                 views.SearchDatinTag, basename='dating Tag')
-# router.register(rf'{endpoint}/search/advance',
-#                 views.AdvancedSearch, basename='advanced search')
+router.register(rf'{endpoint}/search/site',
+                views.SiteSearchViewSet, basename='search site')
+router.register(rf'{endpoint}/search/image',
+                views.SearchBoundingBoxImageViewSet, basename='bounding box image')
+router.register(rf'{endpoint}/search/type',
+                views.TypeSearchViewSet, basename='site type')
+router.register(rf'{endpoint}/search/keywords',
+                views.SearchKeywords, basename='keywords')
+router.register(rf'{endpoint}/search/carving',
+                views.SearchRockCarving, basename='search rock carving')
+router.register(rf'{endpoint}/search/author',
+                views.SearchPeople, basename='search for image creator')
+router.register(rf'{endpoint}/search/institution',
+                views.SearchInstitution, basename='institution')
+router.register(rf'{endpoint}/search/dating',
+                views.SearchDatinTag, basename='dating Tag')
+router.register(rf'{endpoint}/search/advance',
+                views.AdvancedSearch, basename='advanced search')
 # General search url
-# router.register(rf'{endpoint}/search', views.GeneralSearch, basename='search')
+router.register(rf'{endpoint}/search', views.GeneralSearch, basename='search')
 
 # urls for the image gallery
-# router.register(rf'{endpoint}/gallery',
-#                 views.GalleryViewSet, basename='gallery')
+router.register(rf'{endpoint}/gallery',
+                views.GalleryViewSet, basename='gallery')
 
 # urls for the 3D models
-# router.register(rf'{endpoint}/visualization_groups',
-#                 views.VisualizationGroupViewset, basename='Visualization 3D models')
-# router.register(rf'{endpoint}/shfa3d',
-#                 views.SHFA3DViewSet, basename='3D models')
-# router.register(rf'{endpoint}/shfa3dmesh',
-#                 views.SHFA3DMeshViewset, basename='3D meshes')
-# router.register(rf'{endpoint}/geology',
-#                 views.GeologyViewSet, basename='geology')
-# router.register(rf'{endpoint}/camerameta',
-#                 views.CameraSpecificationViewSet, basename='camera meta data')
-# router.register(rf'{endpoint}/null_visualization_group',
-#                 views.NullVisualizationGroupViewset, basename='Null Visualization images')    
+router.register(rf'{endpoint}/visualization_groups',
+                views.VisualizationGroupViewset, basename='Visualization 3D models')
+router.register(rf'{endpoint}/shfa3d',
+                views.SHFA3DViewSet, basename='3D models')
+router.register(rf'{endpoint}/shfa3dmesh',
+                views.SHFA3DMeshViewset, basename='3D meshes')
+router.register(rf'{endpoint}/geology',
+                views.GeologyViewSet, basename='geology')
+router.register(rf'{endpoint}/camerameta',
+                views.CameraSpecificationViewSet, basename='camera meta data')
+router.register(rf'{endpoint}/null_visualization_group',
+                views.NullVisualizationGroupViewset, basename='Null Visualization images')    
 
 # url for contact form
 # router.register(rf'{endpoint}/contact', views.ContactFormViewSet, basename='contact')
